05ccf892f59ef966e3381c7cdf03cc4ba6817d24
rhythmpatel12 <rhythm.patel12@gmail.com>
2024-02-24 11:42:18 -0500

    Enhance ball movement and collision detection with paddles and boundaries
    Introduced comprehensive game mechanics for ball movement, including dynamic bouncing behavior and collision detection with y boundaries and paddles. The ball now reverses direction upon hitting the top or bottom of the play area, and paddle interactions have been refined to change the ball's trajectory based on paddle collisions. These updates contribute to the refinement of the game mechanics.

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ from camera_capture import CameraCapture
 from hand_detection import HandDetection
 from image_processor import load_image
 import numpy as np
+import random
 
 
 class Game:
@@ -11,6 +12,10 @@ class Game:
         self.camera = CameraCapture()
         self.handDetection = HandDetection(detectionCon=0.8, maxHands=2)
         self.load_resources()
+
+        self.ball_position = [615,200]
+        self.speedX = -10
+        self.speedY = -10
 
     def load_resources(self):
         self.imgBg = load_image("resources/Background.png")
@@ -33,7 +38,7 @@ class Game:
             frameBg = cv2.addWeighted(processed_frame, 0.2, self.imgBg, 0.8, 0)
             
             #Game logic 
-
+            #-------------------------------------------------
             #check for hands and add paddle
             if hands: 
                 for hand in hands:
@@ -47,12 +52,32 @@ class Game:
 
                     if hand['type'] == "Left":
                         frameBg = cvzone.overlayPNG(frameBg, self.imgPad1, (54,y_hand))
+                        if 59 < self.ball_position[0] < 59 + w1 and y_hand < self.ball_position[1] < y_hand + h1:
+                            self.speedX = -1 * self.speedX
+                            self.ball_position[0] += 15
 
                     if hand['type'] == "Right":
                         frameBg = cvzone.overlayPNG(frameBg, self.imgPad2, (1190,y_hand))
+                        if 1190 - 50 < self.ball_position[0] < 1190 and y_hand < self.ball_position[1] < y_hand + h1:
+                            self.speedX = -1 * self.speedX
+                            self.ball_position[0] -= 15
+                            
+
+
+            #move the ball 
+            
+            #checking for y boundaries 
+            if self.ball_position[1] >= 500 or self.ball_position[1] < 15:
+                self.speedY = -1 * self.speedY
+
+
+            self.ball_position[0] += self.speedX
+            self.ball_position[1] += self.speedY
 
             #add ball to frame 
-            frameBg = cvzone.overlayPNG(frameBg, self.imgBall, (615,200))
+            frameBg = cvzone.overlayPNG(frameBg, self.imgBall, self.ball_position)
+            #-------------------------------------------------
+
 
             cv2.imshow('Game', frameBg)
             if cv2.waitKey(1) & 0xFF == ord('q'):
